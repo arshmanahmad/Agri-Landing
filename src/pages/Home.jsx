d41e5563e5b7640030527fb2d0a0b799249b27fb
arshmanahmad <132290386+arshmanahmad@users.noreply.github.com>
2024-05-26 17:38:12 -0500

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
         gsap.to('.paras', { opacity: 1, duration: 1 });
       },
 
-      //
+      //aa
       scrollTrigger: {
         trigger: '.paras',
         start: 'top 80%',
